9588f643a95389ed7bf15bb3125f87daa8d324c1
Fernando Cagua <efcaguab@gmail.com>
2017-04-07 13:40:36 +1200

    Skeleton for codefile reading function

---
+++ R/read_codefile.R
@@ -0,0 +1,20 @@
+#' Read a CPC code file
+#'
+#' @param file
+#' @param encoding
+#' @param fileEncoding
+#'
+#' @return a list with two data frames describing the major and the minor categories
+#' @export
+#'
+#' @examples
+read_codefile <- function(file, encoding = "unknown", fileEncoding = "") {
+
+	# object to be returned
+	list(major_categ = data.frame(major_code = NULL,
+																major_name = NULL,
+																col = NULL),
+			 minor_categ = data.frame(minor_code = NULL,
+			 												  minor_name = NULL,
+			 												  major_code = NULL))
+}
